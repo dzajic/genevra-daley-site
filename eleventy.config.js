337fceded76daf7b506b1c76ee771e25971d4bca
Daniel Zajic <danielzajic@gmail.com>
2026-07-23 09:48:19 -0400

    Support GitHub Pages project URLs

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,8 +1,14 @@
+import { EleventyHtmlBasePlugin } from "@11ty/eleventy";
+
 export default function (eleventyConfig) {
+  const pathPrefix = process.env.ELEVENTY_PATH_PREFIX || "/";
+
+  eleventyConfig.addPlugin(EleventyHtmlBasePlugin, { baseHref: pathPrefix });
   eleventyConfig.addPassthroughCopy({ "assets/images": "assets/images" });
   eleventyConfig.addPassthroughCopy({ "src/assets": "assets" });
 
   return {
+    pathPrefix,
     dir: {
       input: "src",
       includes: "_includes",
